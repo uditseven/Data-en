fa03605224bf384347abc37f4008bdb7698f7f7a
Udit Pandey <32841086+uditseven@users.noreply.github.com>
2018-11-03 05:21:03 -0700

    This file contains codes for Signup page
    This file has codes for the page which allows a user to enter first time registration details with unique phone number.

---
+++ Form4.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Threading;
+using System.Drawing.Printing;
+using System.IO;
+using System.Data.OleDb;
+using System.Text.RegularExpressions;
+using System.Data.Odbc;
+using Microsoft.VisualStudio.Data;
+using System.Windows.Forms.DataVisualization.Charting; 
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form4 : Form
+    {
+        public Form4()
+        {
+            InitializeComponent();
+        }
+
+        private void sbmt_sgnup_Click(object sender, EventArgs e)
+        {
+            SqlConnection lsConnstr = new SqlConnection("Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=EX;Data Source=HP-UP\\SQLEXPRESS");
+            SqlCommand cmd;
+            lsConnstr.Open();
+            string s = "insert into T_Login values(@usr_T_box_sgnup,@pwd_T_box_sgnup,@phn_T_box_sgnup)";
+            cmd = new SqlCommand(s, lsConnstr);
+            cmd.Parameters.AddWithValue("@usr_T_box_sgnup", usr_T_box_sgnup.Text);
+            cmd.Parameters.AddWithValue("@pwd_T_box_sgnup", pwd_T_box_sgnup.Text);
+            cmd.Parameters.AddWithValue("@phn_T_box_sgnup", phn_T_box_sgnup.Text);
+            cmd.CommandType = CommandType.Text;
+
+            int i = cmd.ExecuteNonQuery();
+            lsConnstr.Close();
+
+            {
+                MessageBox.Show("Congratulations !! On your NEW Signup ");
+            }
+        }
+    }
+}
