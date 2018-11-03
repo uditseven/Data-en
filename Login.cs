4b1a63d72f73a0a61aeaa28a76b04e829fb1edb2
Udit Pandey <32841086+uditseven@users.noreply.github.com>
2018-11-03 05:04:02 -0700

    This file has codes of login page.
    In login page user can actually login through login credentials. If user is new to application he can signup by clicking signup option or if he has forgotten his credential he can click on forgot password option, then registered login credentials will be send to registered phone number( for this feature user has to activate internet connection on his desktop).

---
+++ Login.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+
+
+namespace WindowsFormsApplication1
+{
+    public partial class Login : Form
+    {
+        dbClss gObjDBClss = new dbClss();
+        DataSet gObjDS = new DataSet();
+        public Login()
+        {
+            InitializeComponent();
+        }
+        private void loginB_Click(object sender, EventArgs e)
+        {
+            if ((usernm.Text.Equals("")) || (pass1.Text.Equals("")))
+            {
+                MessageBox.Show("Please enter username and password");
+            }
+            else
+            {
+                string lsQuery = "Select username from T_Login where Username='" + usernm.Text + "' and pwd='" + pass1.Text + "'";
+                gObjDS = gObjDBClss.GetData(lsQuery);
+                if (gObjDS.Tables[0].Rows.Count > 0)
+                {
+                    Form1 lObjFrm1 = new Form1(usernm.Text);
+                    lObjFrm1.ShowDialog();
+                }
+                else
+                    MessageBox.Show("Enter valid username and password");
+            }
+        }
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Form4 frm4 = new Form4();
+            frm4.ShowDialog();           
+        }
+        private void frgt_pwd_lbl_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Form2 frm2 = new Form2();
+            frm2.ShowDialog();
+        }        
+    }
+}
