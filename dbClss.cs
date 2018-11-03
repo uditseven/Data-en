d0330709d8375b5f9e93dd4cc98e3b5827bb89e7
Udit Pandey <32841086+uditseven@users.noreply.github.com>
2018-11-03 05:31:33 -0700

    This file contains codes for using sql database.
    This file contains codes for using already created SQL database for storing demographic data through this windows application. If one modifies previously entered demographic data through this windows application, the data in SQL database also gets modified.

---
+++ dbClss.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApplication1
+{
+    class dbClss
+    {
+        SqlConnection gObjConn;
+        SqlCommand gObjCmd;
+        DataSet gObjDS;
+        String gsSQLQuery = "";
+        SqlDataAdapter gObjAdpt;
+
+        public string GetConnStr()
+        {
+            string lsConnstr = @"Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=EX;Data Source=HP-UP\SQLEXPRESS";
+            return lsConnstr;
+        }
+        public DataSet GetData(String isSqlQuery)
+        {
+            gObjConn = new SqlConnection(GetConnStr());
+            gObjConn.Open();
+            gsSQLQuery = isSqlQuery;
+            gObjCmd = new SqlCommand(gsSQLQuery, gObjConn);
+            gObjAdpt = new SqlDataAdapter(gObjCmd);
+            gObjDS = new DataSet();
+            gObjAdpt.Fill(gObjDS);
+            return gObjDS;
+
+        }
+        public void InsertUpdateData(String isSqlQuery)
+        {
+            gObjConn = new SqlConnection(GetConnStr());
+            gObjConn.Open();
+            gsSQLQuery = isSqlQuery;
+            gObjCmd = new SqlCommand(gsSQLQuery, gObjConn);
+            gObjCmd.ExecuteNonQuery();
+        }
+    }
+}
